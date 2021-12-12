a69cac5eb04778d1438efc467285f96dd805c064
rjone143 <rjone143@jh.edu>
2021-12-12 13:11:43 -0800

    edits

--- Module10/spec/dish.service.spec.js
+++ Module10/spec/dish.service.spec.js
@@ -17,12 +17,12 @@ describe('testFavirteItemExists', function () {
   
     it('should return a User Info with favarite dish', function() {
       // mock the api call
-      $httpBackend.whenGET(ApiPath + '/menu_items/L1.json').respond({"id":193,"short_name":"L1","name":"Orange Chicken"});
+      $httpBackend.whenGET(ApiPath + '/menu_items/A1.json').respond({"id":1,"short_name":"A1","name":"Won Ton Soup with Chicken"});
       // mock the registration
-      service.userInfo = { lastname: 'aaa', firstname: 'aaa', phone: '111-222-3333', email: 'a@a.a', dish: 'L1' }
+      service.userInfo = { lastname: 'Smith', firstname: 'John', phone: '000-000-0000', email: 'china@pho.com', dish: 'A1' }
       // test get getMenuItem to make sure it is returning the data from api, and this is a step of getUserInfo()
       service.getMenuItem().then(function(response) {
-        expect(response.data).toEqual({"id":193,"short_name":"L1","name":"Orange Chicken"});
+        expect(response.data).toEqual({"id":1,"short_name":"A1","name":"Won Ton Soup with Chicken"});
       });
       // after we made sure api returns correct data, now we test the function is returning the expected user info
       var result = service.getUserInfo()
